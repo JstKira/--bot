9944291287b087d200109e36ae4a2722dcc27d1c
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-05-17 16:50:07 +0300

    Update tools-script.js

--- plugins/tools-script.js
+++ plugins/tools-script.js
@@ -5,15 +5,15 @@ let handler = async function (m, { conn, __dirname }) {
 let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
   
 m.reply(`
-*≡ SCRIPT*
+*≡ لايوجد اسكربت قيد التطوير*
 
 ▢ Git : ${_package.homepage}
 `.trim())
     
 }
 
-handler.help = ['script']
+handler.help = ['اسكربت']
 handler.tags = ['tools']
-handler.command = ['sc', 'git', 'script'] 
+handler.command = ['سكربت', 'git', 'script'] 
 
 export default handler
