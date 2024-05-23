0e072e79a1d39ec2872f19461b635f68732f2195
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-05-23 12:24:08 +0300

    Update اغنيه.js

--- plugins/اغنيه.js
+++ plugins/اغنيه.js
@@ -1,78 +1,108 @@
+import yts from  yt-search 
+import ytdl from  ytdl-core 
+import fs from  fs 
+import { pipeline } from  stream 
+import { promisify } from  util 
+import os from  os 
+import fg from  api-dylux 
+import fetch from  node-fetch 
+let limit = 320
+let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
+  
+    if (!text) throw `✳️ ${mssg.example} *${usedPrefix + command}* Lil Peep hate my life`
+  let chat = global.db.data.chats[m.chat]
+  let res = await yts(text)
+  //let vid = res.all.find(video => video.seconds < 3600)
+  let vid = res.videos[0]
+  if (!vid) throw `✳️ Vídeo/Audio no encontrado`
+  let isVideo = /vid$/.test(command)
+  m.react( 🎧 ) 
+  
+  let play = `
+	≡ *FG MUSIC*
+┌──────────────
+▢ 📌 *${mssg.title}:* ${vid.title}
+▢ 📆 *${mssg.aploud}:* ${vid.ago}
+▢ ⌚ *${mssg.duration}:* ${vid.timestamp}
+▢ 👀 *${mssg.views}:* ${vid.views.toLocaleString()}
+└──────────────
 
-import ytdl from  ytdl-core ;
-import yts from  yt-search ;
-import fs from  fs ;
-import { pipeline } from  stream ;
-import { promisify } from  util ;
-import os from  os ;
+_Enviando..._` 
+conn.sendFile(m.chat, vid.thumbnail,  play , play, m, null, rcanal)
+  
+  let q = isVideo ?  360p  :  128kbps  
+try {
+	
+ // let api = await fetch(global.API( fgmods , `/api/downloader/${isVideo ? "ytv" : "yta"}`, { url: vid.url, quality: q},  apikey ))
+ // let yt = await api.json()
+  
+   let yt = await (isVideo ? fg.ytv : fg.yta)(vid.url, q)
+  let { title, dl_url, quality, size, sizeB } = yt
+  let isLimit = limit * 1024 < sizeB 
+
+     await conn.loadingMsg(m.chat,  📥 Descargando , ` ${isLimit ? `≡  *FG YTDL*\n\n▢ *⚖️${mssg.size}*: ${size}\n▢ *🎞️${mssg.quality}*: ${quality}\n\n▢ _${mssg.limitdl}_ *+${limit} MB*` :  ✅ Descarga Completada  }`, ["▬▭▭▭▭▭", "▬▬▭▭▭▭", "▬▬▬▭▭▭", "▬▬▬▬▭▭", "▬▬▬▬▬▭", "▬▬▬▬▬▬"], m)
+     
+	  if(!isLimit) conn.sendFile(m.chat, dl_url, title +  .mp  + (3 + /vid$/.test(command)), `
+ ≡  *FG YTDL*
+  
+▢ *📌Título* : ${title}
+▢ *🎞️Calidad* : ${quality}
+▢ *⚖️Peso* : ${size}
+`.trim(), m, false, { mimetype: isVideo ?    :  audio/mpeg , asDocument: chat.useDocument })
+		m.react(done) 
+  } catch {
+  try {
+//  let q = isVideo ?  360p  :  128kbps  
+  let yt = await (isVideo ? fg.ytmp4 : ytmp3)(vid.url, q)
+  let { title, dl_url, quality, size, sizeB} = yt
+  let isLimit = limit * 1024 < sizeB 
+
+     await conn.loadingMsg(m.chat,  📥 Descargando , ` ${isLimit ? `≡  *FG YTDL*\n\n▢ *⚖️${mssg.size}*: ${size}\n▢ *🎞️${mssg.quality}*: ${quality}\n\n▢ _${mssg.limitdl}_ *+${limit} MB*` :  ✅ Descarga Completada  }`, ["▬▭▭▭▭▭", "▬▬▭▭▭▭", "▬▬▬▭▭▭", "▬▬▬▬▭▭", "▬▬▬▬▬▭", "▬▬▬▬▬▬"], m)
+	  if(!isLimit) conn.sendFile(m.chat, dl_url, title +  .mp  + (3 + /2$/.test(command)), `
+ ≡  *FG YTDL 2*
+  
+*📌${mssg.title}* : ${title}
+*🎞️${mssg.quality}* : ${quality}
+*⚖️${mssg.size}* : ${size}
+`.trim(), m, false, { mimetype: isVideo ?    :  audio/mpeg , asDocument: chat.useDocument })
+		m.react(done) 
+		
+		 } catch (error) {
+        m.reply(`❎ ${mssg.error}`)
+    }
+}
+
+}
+handler.help = [ ,اغنيه, ]
+handler.tags = [ dl ]
+handler.command = [ شغل3 ,  playvid ]
+handler.disabled = true
+
+export default handler
 
 const streamPipeline = promisify(pipeline);
 
-var handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `مثال : \n ${usedPrefix}${command} midle of night`;
-
-  let search = await yts(text);
-  let vid = search.videos[Math.floor(Math.random() * search.videos.length)];
-  if (!search) throw  Video Not Found, Try Another Title ;
-  let { title, thumbnail, timestamp, views, ago, url } = vid;
-  let wm =   *لوسيفار*  ; //حط اسم بوتك
-
-  let captvid = `  جاري التحميل `;
-
-  conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author }, { quoted: m });
-
-
-  const audioStream = ytdl(url, {
-    filter:  audioonly ,
-    quality:  highestaudio ,
-  });
-
-  // Get the path to the system s temporary directory
-  const tmpDir = os.tmpdir();
-
-  // Create writable stream in the temporary directory
-  const writableStream = fs.createWriteStream(`${tmpDir}/${title}.mp3`);
-
-  // Start the download
-  await streamPipeline(audioStream, writableStream);
-
-  let doc = {
-    audio: {
-      url: `${tmpDir}/${title}.mp3`
-    },
-    mimetype:  audio/mp4 ,
-    fileName: `${title}`,
-    contextInfo: {
-      externalAdReply: {
-        showAdAttribution: true,
-        mediaType: 2,
-        mediaUrl: url,
-        title: title,
-        body: wm,
-        sourceUrl: url,
-        thumbnail: await (await conn.getFile(thumbnail)).data
-      }
-    }
-  };
-
-  await conn.sendMessage(m.chat, doc, { quoted: m });
-
-  // Delete the audio file
-  fs.unlink(`${tmpDir}/${title}.mp3`, (err) => {
-    if (err) {
-      console.error(`Failed to delete audio file: ${err}`);
-    } else {
-      console.log(`Deleted audio file: ${tmpDir}/${title}.mp3`);
-    }
-  });
-};
-
-handler.help = [ اغنيه ].map((v) => v +   <اسم الاغنيه> );
-handler.tags = [ downloader ];
-handler.command = [ شغل1 ,  songs ,  ytmp3doc , اغنيه ]
-
-handler.exp = 0;
-handler.diamond = false;
-
-export default handler;
+async function ytmp3(url) {
+    const videoInfo = await ytdl.getInfo(url);
+    const { videoDetails } = videoInfo;
+    const { title, thumbnails, lengthSeconds, viewCount, uploadDate } = videoDetails;
+    const thumbnail = thumbnails[0].url;
     
+    const audioStream = ytdl(url, { filter:  audioonly , quality:  highestaudio  });
+    const tmpDir = os.tmpdir();
+    const audioFilePath = `${tmpDir}/${title}.mp3`;
+
+    await streamPipeline(audioStream, fs.createWriteStream(audioFilePath));
+
+    return {
+        title,
+        views: viewCount,
+        publish: uploadDate,
+        duration: lengthSeconds,
+        quality:  128kbps ,
+        thumb: thumbnail,
+        size:  0mb , 
+        sizeB:  0 , 
+        dl_url: audioFilePath
+    };
+}
