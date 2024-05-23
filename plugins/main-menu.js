d454b81beeea64dccfe10a4d6aa39f1b86680ba3
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-05-24 00:39:45 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,35 +1,36 @@
-//import db from '../lib/database.js'
-import { promises } from 'fs'
-import { join } from 'path'
-import fetch from 'node-fetch'
-import { xpRange } from '../lib/levelling.js'
-//import { plugins } from '../lib/plugins.js'
+
+//import db from  ../lib/database.js 
+import { promises } from  fs 
+import { join } from  path 
+import fetch from  node-fetch 
+import { xpRange } from  ../lib/levelling.js 
+//import { plugins } from  ../lib/plugins.js 
 let tags = {
-  'main': 'معلومات البوت',
-  'game': 'قسم الألعاب',
-  'econ': 'قسم البنك',
-  'rg': 'قسم التسجيل',
-  'sticker': 'قسم الملصقات',
-  'img': 'قسم الصور',
-  'maker': 'توليد الصور',
-  'group': 'قسم الجروب', 
-  'nime': 'قسم الإنمي',
-  'rnime': 'منشن انمي',
-  'dl': 'قسم التنزيلات',
-  'tools': 'قسم الادوات',
-  'fun': 'قسم الترفيه', 
-  'owner': 'قسم المطور', 
+   main :  معلومات البوت ,
+   game :  قسم الألعاب ,
+   econ :  قسم البنك ,
+   rg :  قسم التسجيل ,
+   sticker :  قسم الملصقات ,
+   img :  قسم الصور ,
+   maker :  توليد الصور ,
+   group :  قسم الجروب , 
+   nime :  قسم الإنمي ,
+   rnime :  منشن انمي ,
+   dl :  قسم التنزيلات ,
+   tools :  قسم الادوات ,
+   fun :  قسم الترفيه , 
+   owner :  قسم المطور , 
 }
 const defaultMenu = {
   before: `
  
-*━⊱│✫ -『 𝗟𝐔𝐂𝐈𝐅𝐀𝐑 𝐁𝐎𝐓 』- ✫│⊱━*\n 
-        *𝐁𝐎𝐓 𝗟𝐔𝐂𝐈𝐅𝐀𝐑🇵🇸*
- ╭━━━━⊱『 *𝑴𝒊𝒓𝒛𝒂* 』⊱━━━━━╮
+*━⊱│✫ -『 𝐁𝐎𝐓 -𝗟𝐔𝐂𝐈𝐅𝐀𝐑 』- ✫│⊱━*\n 
+        *𝗟𝐔𝐂𝐈𝐅𝐀𝐑-𝐁𝐎𝐓*
+ ╭━━━━⊱『 *اهلا بك* 』⊱━━━━━╮
 > *👋🏻 مرحباَ:* *%name*\n> *👥 عدد المستخدمين:* *%totalreg*\n> *🟢 وقت النشاط:* %muptime
 ╯━━━━━━━━━━━━━━━━━╰
 %readmore
-≡ * 𝗟𝐔𝐂𝐈𝐅𝐀𝐑 | 𝐁𝐎𝐓*
+≡ *𝐌𝐢𝐫𝐳𝐚 | 𝐌𝐞𝐧𝐮*
 `.trimStart(),
   header: "╮━━⊱ 「 *%category* 」⊱━━╭",
  body: "  │✫ -  %cmd %isdiamond %isPremium",
@@ -39,39 +40,39 @@ const defaultMenu = {
 }
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   try {
-    let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
+    let _package = JSON.parse(await promises.readFile(join(__dirname,  ../package.json )).catch(_ => ({}))) || {}
     let { exp, diamond, level, role } = global.db.data.users[m.sender]
     let { min, xp, max } = xpRange(level, global.multiplier)
     let name = await conn.getName(m.sender)
     let d = new Date(new Date + 3600000)
-    let locale = 'es'
+    let locale =  es 
     // d.getTimeZoneOffset()
     // Offset -420 is 18.00
     // Offset    0 is  0.00
     // Offset  420 is  7.00
-    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
-    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let weton = [ Pahing ,  Pon ,  Wage ,  Kliwon ,  Legi ][Math.floor(d / 84600000) % 5]
+    let week = d.toLocaleDateString(locale, { weekday:  long  })
     let date = d.toLocaleDateString(locale, {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
+      day:  numeric ,
+      month:  long ,
+      year:  numeric 
     })
-    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
+    let dateIslamic = Intl.DateTimeFormat(locale +  -TN-u-ca-islamic , {
+      day:  numeric ,
+      month:  long ,
+      year:  numeric 
     }).format(d)
     let time = d.toLocaleTimeString(locale, {
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric'
+      hour:  numeric ,
+      minute:  numeric ,
+      second:  numeric 
     })
     let _uptime = process.uptime() * 1000
     let _muptime
     if (process.send) {
-      process.send('uptime')
+      process.send( uptime )
       _muptime = await new Promise(resolve => {
-        process.once('message', resolve)
+        process.once( message , resolve)
         setTimeout(resolve, 1000)
       }) * 1000
     }
@@ -83,14 +84,14 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       return {
         help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
         tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
-        prefix: 'customPrefix' in plugin,
+        prefix:  customPrefix  in plugin,
         diamond: plugin.diamond,
         premium: plugin.premium,
         enabled: !plugin.disabled,
       }
     })
     for (let plugin of help)
-      if (plugin && 'tags' in plugin)
+      if (plugin &&  tags  in plugin)
         for (let tag of plugin.tags)
           if (!(tag in tags) && tag) tags[tag] = tag
     conn.menu = conn.menu ? conn.menu : {}
@@ -98,30 +99,30 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     let header = conn.menu.header || defaultMenu.header
     let body = conn.menu.body || defaultMenu.body
     let footer = conn.menu.footer || defaultMenu.footer
-    let after = conn.menu.after || (conn.user.jid == conn.user.jid ? '' : `⭐ Powered by 𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕 https://wa.me/${conn.user.jid.split`@`[0]}`) + defaultMenu.after
+    let after = conn.menu.after || (conn.user.jid == conn.user.jid ?    : `⭐ Powered by 𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕 https://wa.me/${conn.user.jid.split`@`[0]}`) + defaultMenu.after
     let _text = [
       before,
       ...Object.keys(tags).map(tag => {
-        return header.replace(/%category/g, tags[tag]) + '\n' + [
+        return header.replace(/%category/g, tags[tag]) +  \n  + [
           ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
             return menu.help.map(help => {
-              return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
-                .replace(/%isdiamond/g, menu.diamond ? '(ⓓ)' : '')
-                .replace(/%isPremium/g, menu.premium ? '(Ⓟ)' : '')
+              return body.replace(/%cmd/g, menu.prefix ? help :  %p  + help)
+                .replace(/%isdiamond/g, menu.diamond ?  (ⓓ)  :   )
+                .replace(/%isPremium/g, menu.premium ?  (Ⓟ)  :   )
                 .trim()
-            }).join('\n')
+            }).join( \n )
           }),
           footer
-        ].join('\n')
+        ].join( \n )
       }),
       after
-    ].join('\n')
-    let text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
+    ].join( \n )
+    let text = typeof conn.menu ==  string  ? conn.menu : typeof conn.menu ==  object  ? _text :   
     let replace = {
-      '%': '%',
+       % :  % ,
       p: _p, uptime, muptime,
       me: conn.getName(conn.user.jid),
-      sbot: (conn.user.jid == global.conn.user.jid ? '' : `\n▢ ✨ *Sub-Bot de:*\nwa.me/${global.conn.user.jid.split`@`[0]}`), 
+      sbot: (conn.user.jid == global.conn.user.jid ?    : `\n▢ ✨ *Sub-Bot de:*\nwa.me/${global.conn.user.jid.split`@`[0]}`), 
       npmname: _package.name,
       npmdesc: _package.description,
       version: _package.version,
@@ -129,30 +130,30 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       maxexp: xp,
       totalexp: exp,
       xp4levelup: max - exp,
-      github: _package.homepage ? _package.homepage.url || _package.homepage : '[unknown github url]',
+      github: _package.homepage ? _package.homepage.url || _package.homepage :  [unknown github url] ,
       level, diamond, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
       readmore: readMore
     }
-    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`,  g ), (_, name) =>    + replace[name])
     
-    let pp = './src/fg_logo.jpg'
+    let pp =  ./src/fg_logo.jpg 
 
       conn.sendButton2(m.chat, rcanal, text.trim(), `▢ 𝑴𝒊𝒓𝒛𝒂 ┃ ᴮᴼᵀ\n${mssg.ig}`, pp [
-      ['⏍ معلومات البوت', `${_p}botinfo`],
-      ['⌬ الدعم', `${_p}support`]
+      [ ⏍ معلومات البوت , `${_p}botinfo`],
+      [ ⌬ الدعم , `${_p}support`]
     ], m, rpl)
-    conn.sendFile(m.chat, pp, 'menu.jpg', text.trim(), m, null, rcanal)
+    conn.sendFile(m.chat, pp,  menu.jpg , text.trim(), m, null, rcanal)
   
-    m.react('✨') 
+    m.react( ✨ ) 
     
   } catch (e) {
-    conn.reply(m.chat, '❎ Lo sentimos, el menú tiene un error', m)
+    conn.reply(m.chat,  ❎ Lo sentimos, el menú tiene un error , m)
     throw e
   }
 }
-//handler.help = ['help']
-//handler.tags = ['main']
-handler.command = ['اوامر',| 'أوامر’ |’المهام'] 
+//handler.help = [ help ]
+//handler.tags = [ main ]
+handler.command = [ اوامر ,  أوامر ,  المهام ] 
 handler.register = false
 
 export default handler
@@ -161,9 +162,9 @@ const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
 function clockString(ms) {
-  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, 'd ', h, 'h ', m, 'm '].map(v => v.toString().padStart(2, 0)).join('')
+  let d = isNaN(ms) ?  --  : Math.floor(ms / 86400000)
+  let h = isNaN(ms) ?  --  : Math.floor(ms / 3600000) % 24
+  let m = isNaN(ms) ?  --  : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ?  --  : Math.floor(ms / 1000) % 60
+  return [d,  d  , h,  h  , m,  m  ].map(v => v.toString().padStart(2, 0)).join(  )
 }
